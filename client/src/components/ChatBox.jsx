83c46488bd188c91afbaad92a4d43815008b1a8c
Ivan Ramirez Comas <70542717+IvanR-C@users.noreply.github.com>
2022-07-10 16:30:33 -0400

    worked on messages

--- client/src/components/ChatBox.jsx
+++ client/src/components/ChatBox.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import {
-  Box,
-  TextField,
-  List,
-} from "@mui/material";
-import {chatMessages} from "../fData"
+import { Box, TextField, List, Card, Button, Typography } from "@mui/material";
+import { chatMessages } from "../fData";
 import ListItem from "@mui/material/ListItem";
-import ListItemAvatar from "@mui/material/ListItemAvatar";
 import ListItemText from "@mui/material/ListItemText";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
 
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+  >
+    •
+  </Box>
+);
 
 const ChatBox = () => {
   return (
@@ -16,31 +21,45 @@ const ChatBox = () => {
       sx={{
         display: "flex",
         flexDirection: "column",
-        alignItems: "flex-end",
+
         backgroundColor: "#ffffff",
         borderLeft: "1px solid",
         borderColor: "#f0f0f0",
       }}
     >
-      <List sx={{flexGrow: "1", mb: 2 }}>
-          {chatMessages.map(({ id, sender, msg, timeStamp }) => (
-            <React.Fragment key={id}>
-              <ListItem>
-                <ListItemText primary={sender.name} secondary={msg}  />
-              </ListItem>
-            </React.Fragment>
-          ))}
-        </List>
+      <List sx={{ display: "flex", flexDirection: "column", flexGrow: "1", mb: 2 }}>
+        {chatMessages.map(({ id, sender, msg, timeStamp, position, color }) => (
+            <ListItem key={id} sx={{justifyContent: `${position}`}}>
+              <Card sx={{ backgroundColor: `${color}` }}>
+                <CardContent sx={{padding: "10px", "& .MuiCardContent-root:last-child": {
+                pb: "10px",
+              },}}>
+                  <Typography
+                    sx={{ fontSize: 10 }}
+                    color="text.secondary"
+
+                  >
+                    {timeStamp}
+                  </Typography>
+                  <Typography sx={{ fontSize: 12, fontWeight: 'medium' }}
+                   component="div">
+                    {sender.name}
+                  </Typography>
+                  <Typography sx={{fontSize: 11}}>{msg}</Typography>
+                </CardContent>
+              </Card>
+            </ListItem>
+        ))}
+      </List>
 
       <TextField
         sx={{
-          
           padding: "10px",
 
           "& .MuiFilledInput-root": {
             height: "54px",
             borderRadius: "5px",
-            backgroundColor: "#d4d4d4"
+            backgroundColor: "#d4d4d4",
           },
         }}
         id="filled-basic"
--- client/src/fData/messages.jsx
+++ client/src/fData/messages.jsx
@@ -5,34 +5,44 @@ export const chatMessages = [
     msg:
       "Habla matador",
     timeStamp: "20:00",
+    position: "left",
+    color: "#dfe0eb"
   },
   {
     id: 2,
     sender: {id:2, name: "Pepe"},
     msg:
-      "Que vuelta amigo mio",
-    timeStamp: "20:00",
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo ",
+    timeStamp: "20:02",
+    position: "flex-end",
+    color: "#9fa2b4"
   },
   {
     id: 3,
     sender: {id:1, name: "Ediberto"},
     msg:
       "Como esta la cosa",
-    timeStamp: "20:00",
+    timeStamp: "20:03",
+    position: "flex-start",
+    color: "#dfe0eb"
   },
   {
     id: 4,
     sender: {id:2, name: "Pepe"},
     msg:
       "Me gustan las habichuelas",
-    timeStamp: "20:00",
+    timeStamp: "20:04",
+    position: "right",
+    color: "#9fa2b4"
   },
   {
     id: 5,
     sender: {id:1, name: "Ediberto"},
     msg:
       "Son de la loma",
-    timeStamp: "20:00",
+    timeStamp: "20:15",
+    position: "left",
+    color: "#dfe0eb"
   },]
 
   
